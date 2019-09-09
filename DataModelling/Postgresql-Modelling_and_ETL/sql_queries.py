505367d747f36474866676d0aa7ff964c2040ef6
nj11 <natashajuneja@gmail.com>
2019-09-09 10:10:36 -0500

    Changed on conflict clause for users table and datatype for session id

--- DataModelling/Postgresql-Modelling_and_ETL/sql_queries.py
+++ DataModelling/Postgresql-Modelling_and_ETL/sql_queries.py
@@ -16,7 +16,7 @@ create table IF NOT EXISTS songplays(
         level text ,
         song_id text REFERENCES songs(song_id) ,
         artist_id text ,
-        session_id text ,
+        session_id int ,
         location text,
         user_agent text
   ) 
@@ -74,8 +74,11 @@ songplay_table_insert = ("""
 user_table_insert = ("""
     INSERT INTO users
     (user_id, first_name, last_name, gender, level)
-    VALUES (%s, %s, %s, %s, %s)
-    ON CONFLICT (user_id) DO NOTHING;  
+     VALUES (%s, %s, %s, %s, %s)
+     ON CONFLICT (user_id) DO UPDATE SET 
+     first_name=EXCLUDED.first_name,
+     last_name=EXCLUDED.last_name, gender=EXCLUDED.gender,level=EXCLUDED.level;
+   
 """)
 
 song_table_insert = ("""
