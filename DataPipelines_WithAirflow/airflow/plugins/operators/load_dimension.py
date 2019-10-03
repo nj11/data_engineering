bdca90379b2deafb9c3293e4895a953f7b212d00
Natasha Juneja <natashajuneja@gmail.com>
2019-10-03 10:13:40 -0500

    Added docstring

--- DataPipelines_WithAirflow/airflow/plugins/operators/load_dimension.py
+++ DataPipelines_WithAirflow/airflow/plugins/operators/load_dimension.py
@@ -3,7 +3,7 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-
+   ''' Reusable operator to load data into dimension tables in data warehouse '''
     ui_color = '#80BD9E'
 
     insert_sql = """
@@ -26,6 +26,7 @@ class LoadDimensionOperator(BaseOperator):
         self.load_sql_stmt = load_sql_stmt
         
     def execute(self, context):
+        '''On run, delete dim table and reload data from insert SQL query and table name passed as an argument '''
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
         redshift.run("DELETE FROM {}".format(self.table))
         self.log.info("Deleted table : {}".format(self.table))
